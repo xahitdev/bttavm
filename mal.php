a9680925f2ae5d7d0a3b4e893caebde88cddaf4b
xahitdev <113922120+xahitdev@users.noreply.github.com>
2025-02-22 18:11:24 +0300

    updat

---
+++ mal.php
@@ -0,0 +1,33 @@
+<div class="col-lg-6">
+    <div class="register-form">
+        <div class="row">
+            <div class="col-md-6">
+                <label>First Name</label>
+                <input class="form-control" type="text" placeholder="First Name">
+            </div>
+            <div class="col-md-6">
+                <label>Last Name</label>
+                <input class="form-control" type="text" placeholder="Last Name">
+            </div>
+            <div class="col-md-6">
+                <label>E-mail</label>
+                <input class="form-control" type="text" placeholder="E-mail">
+            </div>
+            <div class="col-md-6">
+                <label>Mobile No</label>
+                <input class="form-control" type="text" placeholder="Mobile No">
+            </div>
+            <div class="col-md-6">
+                <label>Password</label>
+                <input class="form-control" type="text" placeholder="Password">
+            </div>
+            <div class="col-md-6">
+                <label>Retype Password</label>
+                <input class="form-control" type="text" placeholder="Password">
+            </div>
+            <div class="col-md-12">
+                <button class="btn">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
--- settings.php
+++ settings.php
@@ -1,3 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bttavm";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected successfully";
 ?>
